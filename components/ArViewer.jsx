6ab3a566388f41a0fc3eb4a9b4878c2d148f98cc
DvKirbo <cuetosalazarsebas@gmail.com>
2024-11-15 20:25:18 -0500

    FUNCIONA CSMRE

--- components/ArViewer.jsx
+++ components/ArViewer.jsx
@@ -1,7 +1,33 @@
-import { Text } from "react-native";
+import { ViroAmbientLight, ViroARScene, ViroARSceneNavigator, ViroBox, ViroTrackingStateConstants } from "@reactvision/react-viro";
+import { Text, View } from "react-native";
 
 export default function ArViewerC (){
+
+    const CardScene = () => {
+        return (
+            <ViroARScene>
+                <ViroAmbientLight color="#ffffff" influenceBitMask={1}/>
+                <ViroBox position={[0, -1, -2]} scale={[.5, .5, .1]}/>
+            </ViroARScene>
+        )
+    }
+
+    const onInitialized = (state, reason) => {
+        if(state === ViroTrackingStateConstants.TRACKING_NORMAL){
+            console.log("Trackeo normal");
+        } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE){
+            console.log("Trackeo no disponible");
+        }
+    }
+
     return (
-        <Text>AR Viewer</Text>
+        <View style={{flex:1, width: "100%", height:"100%"}}>
+            <ViroARSceneNavigator
+                autofocus={true}
+                initialScene={{scene: CardScene}}
+                style={{flex: 1, width: "100%", height: "100%"}}
+                />
+
+        </View>
     );
 }
