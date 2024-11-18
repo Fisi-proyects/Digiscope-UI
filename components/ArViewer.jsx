e572153f3a4e59e60448a9cedc25ee2e6612f588
DvKirbo <cuetosalazarsebas@gmail.com>
2024-11-18 13:30:32 -0500

    models3d

--- components/ArViewer.jsx
+++ components/ArViewer.jsx
@@ -1,5 +1,50 @@
-import { ViroAmbientLight, ViroARScene, ViroARSceneNavigator, ViroBox, ViroTrackingStateConstants } from "@reactvision/react-viro";
+import { Viro3DObject, ViroAmbientLight, ViroARScene, ViroARSceneNavigator, ViroBox, ViroTrackingStateConstants } from "@reactvision/react-viro";
 import { Text, View } from "react-native";
+
+const models = [
+    {
+        target: 'microscope',
+        model: require('../res/microscope/microscope.obj'),
+        type: 'OBJ',
+        scale: [.1, .1, .1],
+        resources: [],
+    },
+    {
+        target: 'matraz',
+        model: require('../res/matraz1/matraz1.obj'),
+        type: 'OBJ',
+        scale: [.1, .1, .1],
+        resources: [],
+    },
+    {
+        target:'mechero',
+        model: require('../res/mechero/mechero.obj'),
+        type: 'OBJ',
+        resources: [],
+        scale: [.1, .1, .1],
+    },
+    {
+        target: 'mezcladora',
+        model: require('../res/mezcladora/mezcladora.obj'),
+        type: 'OBJ',
+        scale: [.1, .1, .1],
+        resources: [],
+    },
+    {
+        target: 'test_tubes',
+        model: require('../res/tubos_ensayo/tubos_ensayo.obj'),
+        type: 'OBJ',
+        scale: [.1, .1, .1],
+        resources: [],
+    },
+    {
+        target:'probeta',
+        model: require('../res/probeta/tubos_medidor.obj'),
+        type: 'OBJ',
+        scale: [.1, .1, .1],
+        resources: [],
+    }
+]
 
 export default function ArViewerC (){
 
@@ -7,7 +52,19 @@ export default function ArViewerC (){
         return (
             <ViroARScene>
                 <ViroAmbientLight color="#ffffff" influenceBitMask={1}/>
-                <ViroBox position={[0, -1, -2]} scale={[.5, .5, .1]}/>
+                <Viro3DObject
+                    type="OBJ"
+                    postion={[0, 0, -1]}
+                    scale={[.1, .1, .1]}
+                    source={require('../res/petri/petri.obj')}
+                    resources={[require('../res/petri/bacteria_image.jpg')]}
+                />
+                <Viro3DObject
+                    type="OBJ"
+                    postion={[0, 2, -1]}
+                    scale={[.1, .1, .1]}
+                    source={require('../res/balanza/Gramera.obj')}
+                />
             </ViroARScene>
         )
     }
