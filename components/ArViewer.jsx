59fba3fa9b9c912d12d56f7b82d02efbce1bd100
DvKirbo <cuetosalazarsebas@gmail.com>
2024-11-19 22:20:12 -0500

    commit changes

--- components/ArViewer.jsx
+++ components/ArViewer.jsx
@@ -1,7 +1,31 @@
 import { Text } from "react-native";
+import BouncyCheckboxGroup, {
+    CheckboxButton,
+  } from "react-native-bouncy-checkbox-group";
 
 export default function ArViewerC (){
     return (
+    <>
         <Text>AR Viewer</Text>
+        <BouncyCheckboxGroup
+  data={[
+    { label: "Option 1", value: "1" },
+    { label: "Option 2", value: "2" },
+    { label: "Option 3", value: "3" },
+  ]}
+/>
+<BouncyCheckboxGroup
+  data={
+    [
+      { label: "Option 1", value: "1" },
+      { label: "Option 2", value: "2" },
+      { label: "Option 3", value: "3" },
+    ]
+  }
+  style={{ flexDirection: "column" }}
+/>
+
+    </>
     );
+    
 }
