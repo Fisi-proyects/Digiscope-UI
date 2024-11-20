bd72c460cefb1ab8e0ccaf0c414e0ab7e48c4e21
Yayir <yayir.mattos@unmsm.edu.pe>
2024-11-19 21:43:43 -0500

    Personalización de Lecciones

--- app/lessons/[id].js
+++ app/lessons/[id].js
@@ -1,5 +1,5 @@
 import { Link, useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, ScrollView, StyleSheet, TouchableOpacity, Platform, StatusBar, View, Image} from "react-native";
 import { lessonsData } from "./LessonsData";
 
 export default function DetailLesson(){
@@ -7,17 +7,103 @@ export default function DetailLesson(){
     const {id} = useLocalSearchParams();
     const lesson = lessonsData.find(lesson => lesson.id === parseInt(id)); 
     return (
-        <View style={{
-            flex:1,
-            justifyContent:'center',
-            alignItems:'center'
-            }}>
-            <Text>
-                {lesson.tool}
-            </Text>
-            <Link href={'/'} >
-                Volver Atras
-            </Link>
+        <View style={styles.container}>
+            <View style={styles.statusBarSpacer} />
+            <ScrollView>
+                <View style={styles.principalContainer}>
+                    <Link href={'/'} >
+                        <View style={styles.buttonBack}>
+                            <Image source={require('../../assets/FlechaAtras.png')} />
+                        </View>
+                    </Link>
+                    <Text style={styles.title}>
+                        Lección N°{id}: {lesson.tool}
+                    </Text>
+                    <Image source={{ uri: lesson.image }} style={styles.lessonImage} resizeMode="contain"/>
+                </View>
+                <View style={styles.subprincipalContainer}>
+                    <Text style={styles.text}>
+                        {lesson.description}
+                    </Text>
+                    <Text style={styles.subtitle}>
+                        Caracteristicas:
+                    </Text>
+                    <Text style={styles.text}>
+                        {lesson.characteristics}
+                    </Text>
+                    <Text style={styles.subtitle}>
+                        Intrucciones de Uso:
+                    </Text>
+                    <Text style={styles.text}>
+                        {lesson.instruction}
+                    </Text>
+                    <Text style={styles.subtitle}>
+                        Precauciones:
+                    </Text>
+                    <Text style={styles.text}>
+                        {lesson.caution}
+                    </Text>
+                </View>
+            </ScrollView>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    statusBarSpacer: {
+        height: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight, // Espaciado dinámico
+    },
+    container: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        paddingTop: 30,
+        paddingBottom: 75,
+    },
+    buttonBack:{
+        backgroundColor: '#959595',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 50,
+        height: 50,
+        borderRadius: 10,
+    },
+    principalContainer:{
+        width: '100%',
+        paddingHorizontal: 25,
+        gap: 10,
+    },
+    subprincipalContainer:{
+        width: '100%',
+        paddingTop: 10,
+        paddingHorizontal: 30,
+        gap: 10,
+    },
+    lessonImage: {
+        width: '100%',
+        height: 150, 
+    },
+    title: {
+        textAlign: 'center',
+        fontFamily: 'Inter',
+        fontSize: 20,
+        lineHeight: 24,
+        fontWeight: 'bold',
+    },
+    subtitle: {
+        textAlign: 'left',
+        fontFamily: 'Inter',
+        fontWeight: 'bold',
+        fontSize: 14,
+        lineHeight: 17,
+    },
+    text: {
+        textAlign: 'left',
+        fontFamily: 'Inter',
+        fontSize: 13,
+        lineHeight: 17,
+    },
+    button: {
+
+    },
+})
